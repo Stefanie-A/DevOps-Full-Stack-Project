0e4a8bfe04b94f5ff0edb56b3b9b5772705e3d0b
Stefanie-A <albertstephanie630@gmail.com>
2025-08-26 14:02:02 +0100

    chore: comment rds details

--- infrastructure/output.tf
+++ infrastructure/output.tf
@@ -5,12 +5,12 @@ output "private_key" {
 output "ec2_instance_id" {
   value = aws_instance.ec2_instance.id
 }
-output "rds_instance_id" {
-  value = aws_db_instance.app_db.id
-}
-output "rds_endpoint" {
-  value = aws_db_instance.app_db.endpoint
-}
+# output "rds_instance_id" {
+#   value = aws_db_instance.app_db.id
+# }
+# output "rds_endpoint" {
+#   value = aws_db_instance.app_db.endpoint
+# }
 output "ec2_public_ip" {
   value = aws_instance.ec2_instance.public_ip
 }
