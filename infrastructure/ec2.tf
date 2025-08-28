4e1e14bd8e7a41f611bae5902e9cf0b7d2ea76b1
Stefanie-A <albertstephanie630@gmail.com>
2025-08-28 13:43:45 +0100

    feat: install docker compose in instance

--- infrastructure/ec2.tf
+++ infrastructure/ec2.tf
@@ -38,6 +38,8 @@ resource "aws_instance" "ec2_instance" {
     curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
     sudo apt-get install -y nodejs
     npm install -g pm2
+    sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    sudo chmod +x /usr/local/bin/docker-compose   
 
   EOF
   tags = {
