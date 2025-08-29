39bd3c98ff5ad095cd09aa482c22e036455aa41a
Stefanie-A <albertstephanie630@gmail.com>
2025-08-29 12:19:52 +0100

    feat: install prometheus and grafana

--- infrastructure/ec2.tf
+++ infrastructure/ec2.tf
@@ -30,6 +30,7 @@ resource "aws_instance" "ec2_instance" {
     sudo apt update -y
     sudo apt upgrade -y
     sudo apt install nginx -y
+    sudo systemctl stop nginx
     sudo apt install -y docker.io
     sudo systemctl start docker
     sudo systemctl enable docker
@@ -102,10 +103,28 @@ resource "aws_instance" "Observability_instance" {
     sudo su
     sudo apt update -y
     sudo apt upgrade -y
-    wget https://github.com/prometheus/prometheus/releases/download/v2.52.0/prometheus-2.52.0.linux-amd64.tar.gz
-    tar xvf prometheus-2.52.0.linux-amd64.tar.gz
-    cd prometheus-2.52.0.linux-amd64
-
+    sudo useradd --no-create-home --shell /bin/false prometheus
+    sudo mkdir /etc/prometheus
+    sudo mkdir /var/lib/prometheus
+    sudo chown prometheus:prometheus /var/lib/prometheus
+    wget https://github.com/prometheus/prometheus/releases/download/v2.53.1/prometheus-2.53.1.linux-amd64.tar.gz
+    tar xvf prometheus-2.53.1.linux-amd64.tar.gz
+    cd prometheus-2.53.1.linux-amd64
+    sudo mv prometheus /usr/local/bin/
+    sudo mv promtool /usr/local/bin/
+    sudo mv consoles /etc/prometheus
+    sudo mv console_libraries /etc/prometheus
+    sudo mv prometheus.yml /etc/prometheus
+    sudo chown -R prometheus:prometheus /etc/prometheus
+    sudo chown prometheus:prometheus /usr/local/bin/prometheus
+    sudo chown prometheus:prometheus /usr/local/bin/promtool
+    sudo apt-get install -y software-properties-common
+    sudo add-apt-repository "deb https://apt.grafana.com stable main"
+    wget -q -O - https://apt.grafana.com/gpg.key | sudo apt-key add -
+    sudo apt-get install grafana -y
+    sudo systemctl enable grafana-server
+    sudo systemctl start grafana-server
+    sudo systemctl status grafana-server
 
   EOF
   associate_public_ip_address = true
